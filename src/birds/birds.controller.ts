c899aeba5822d47cd07cccce7b54acf700f9df6e
Nadim Basalamah <dimbslmh@gmail.com>
2022-10-15 16:28:02 +0700

    Add birds upload

--- src/birds/birds.controller.ts
+++ src/birds/birds.controller.ts
@@ -1,32 +1,43 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
-import { Roles } from "../common/decorators/roles.decorator";
+import { FileInterceptor, FilesInterceptor } from "@nest-lab/fastify-multer";
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors
+} from "@nestjs/common";
+
 import { RolesGuard } from "../common/guards/roles.guard";
-import { ParseIntPipe } from "../common/pipes/parse-int.pipe";
-import { BirdsService } from "./birds.service";
-import { CreateBirdDto } from "./dto/create-bird.dto";
-import { Bird } from "./interfaces/bird.interface";
 
 @UseGuards(RolesGuard)
 @Controller("birds")
 export class BirdsController {
-  constructor(private readonly birdsService: BirdsService) {}
+  constructor() {}
 
-  @Post()
-  @Roles("admin")
-  async create(@Body() createBirdDto: CreateBirdDto) {
-    this.birdsService.create(createBirdDto);
-  }
-
-  @Get()
-  async findAll(): Promise<Bird[]> {
-    return this.birdsService.findAll();
-  }
-
-  @Get(":id")
-  findOne(
-    @Param("id", new ParseIntPipe())
-    id: number
+  @Post("upload/bird")
+  @UseInterceptors(
+    FileInterceptor("bird", {
+      dest: "./upload/bird",
+    })
+  )
+  async uploadFile(
+    @UploadedFile()
+    bird: any
   ) {
-    // get by ID logic
+    console.log(bird);
+  }
+
+  @Post("upload/birds")
+  @UseInterceptors(
+    FilesInterceptor("birds", undefined, {
+      dest: "./upload/birds",
+    })
+  )
+  async uploadFiles(
+    @UploadedFiles()
+    birds: any
+  ) {
+    console.log(birds);
   }
 }
--- src/birds/birds.module.ts
+++ src/birds/birds.module.ts
@@ -1,30 +1,9 @@
-import { FastifyMulterModule } from "@nest-lab/fastify-multer";
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { diskStorage } from "multer";
+
 import { BirdsController } from "./birds.controller";
-import { BirdsService } from "./birds.service";
 
 @Module({
-  imports: [
-    FastifyMulterModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: () => {
-        const storages = {
-          local: () => {
-            return diskStorage({
-              destination: "./files/birds"
-            });
-          }
-        };
-
-        return {
-          storage: storages["local"]()
-        };
-      }
-    })
-  ],
+  imports: [],
   controllers: [BirdsController],
-  providers: [BirdsService]
 })
 export class BirdsModule {}
